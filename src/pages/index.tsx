67abb6a9714bb0c8f2d535252dff893cb7f2a05c
Guilherme <guilhermecarvalhobao@gmail.com>
2022-05-21 16:23:00 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +8,17 @@ export default function Home() {
       <Head>
         <title>Início | wave.news</title>
       </Head>
-      <h1>Hello</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey welcome</span>
+          <h1>Lorem Ipsum is <span>Dummy</span> text.</h1>
+          <p>
+          It has survived not only five <br />
+          <span>for R$1,00 mes</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Maquiadora" />
+      </main>
     </>
   )
 }
